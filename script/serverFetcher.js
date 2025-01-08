6a3990489d1ec8d0d6b47a6624d45f7d820a6c13
Furaken <91683497+Furaken@users.noreply.github.com>
2025-01-08 14:12:35 +0700

    added 2 new functions for serverFetcher.js

--- script/serverFetcher.js
+++ script/serverFetcher.js
@@ -23,10 +23,57 @@ function updateServers() {
         }
     }
 }
-var servers = {},
-    matrixs = ["Garden", "Desert", "Ocean", "Jungle", "Ant Hell", "Hel", "Sewers"]
+
+function getServerByCp6Code(cp6Code) {
+    for (const [biome, serversObj] of Object.entries(servers)) {
+        for (const [server, obj] of Object.entries(serversObj)) {
+            if (Object.keys(obj).includes(cp6Code)) {
+                return {server, biome, cp6Code}
+            }
+        }
+    }
+    return
+}
+
+function getCp6IdByBiome(biome) {
+    let t = {}
+    Object.keys(servers[biome]).forEach(server => {
+        t[server] = Object.keys(servers[biome][server])
+    })
+    return t
+}
+
+let servers = {},
+    matrixs = ["Garden", "Desert", "Ocean", "Jungle", "Ant Hell", "Hel", "Sewers"],
+    totalServers = 7
+
 updateServers()
 setInterval(() => {
     updateServers()
-    getServerId()
 }, 5 * 1000)
+
+/*
+    getCp6IdByBiome(biome) will give you available cp6 codes if a biome
+        example input: getCp6IdByBiome("Ant Hell")
+        example output:
+            {
+                "NA": [
+                    "1gms"
+                ],
+                "EU": [
+                    "1gmt"
+                ],
+                "AS": [
+                    "1gn1"
+                ]
+            }
+        
+    getServerByCp6Code(cp6Code) will give you server object that has your inputed cp6 id
+        example input: getServerByCp6Code("1gms")
+        example ouput:
+            {
+                "server": "NA",
+                "biome": "Ant Hell",
+                "cp6Code": "1gms"
+            }
+*/
