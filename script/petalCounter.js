a42071a5e54fe92653f25a376866e273f750d18c
Furaken <91683497+Furaken@users.noreply.github.com>
2025-01-24 12:28:04 +0700

    1.3.7

--- script/petalCounter.js
+++ script/petalCounter.js
@@ -439,7 +439,7 @@
         document.querySelectorAll('#petalCounter_progress > div > img').forEach(x => {
             x.onclick = function () {
                 if (x.id.startsWith('petalCounter_progress_petal_')) {
-                    let a = x.id.replace('petalCounter_progress_petal_', '').split('_')
+                    let a = x.id.replace('petalCounter_progress_petal_', '')
                     let p = a[0]
                     let b = new Array(kRarity.length).fill(0)
                     let aim = prompt(`Aim (${p})\n(Set to 0 to remove)\n${kRarity.map(x => x.name)}`, lcs_.count.petal[p].toString())
